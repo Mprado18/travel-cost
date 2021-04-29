25e3b184ed08f3e71ce9e39b8290614973f55cd6
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-04-29 16:47:38 -0300

    done the logic of the calculation, event click and input validation

--- app/src/main/java/com/example/travel_cost/MainActivity.kt
+++ app/src/main/java/com/example/travel_cost/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.travel_cost
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.travel_cost.databinding.ActivityMainBinding
+import java.lang.ArithmeticException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.buttonCalculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        if (validation()) {
+            val distance = binding.inputDistance.text.toString().toInt()
+            val priceForLitre = binding.inputPrice.text.toString().toFloat()
+            val autonomy = binding.inputAutonomy.text.toString().toFloat()
+
+            val totalCost = (distance * priceForLitre) / autonomy
+            binding.totalCost.text = "R$ ${"%.2f".format(totalCost)}"
+        } else {
+            Toast.makeText(this, getString(R.string.fill_every_inputs), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun validation(): Boolean {
+        when {
+            binding.inputDistance.text.toString() == "" -> return false
+            binding.inputPrice.text.toString() == "" -> return false
+            binding.inputAutonomy.text.toString() == "" -> return false
+            binding.inputAutonomy.text.toString() == "0" -> {
+                Toast.makeText(this, getString(R.string.error_value), Toast.LENGTH_LONG).show()
+                return false
+            }
+        }
+        return true
     }
 }
